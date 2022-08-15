9717735f1a68e7917961cb2108e47c5e4e89e297
DJ Bates <blooviper@gmail.com>
2022-08-15 09:18:23 -0600

    First Commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2; 
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool TorF)
+        {
+            int answer = num1 + num2;   
+
+            if (TorF)
+            {
+                if (answer > 1)
+                {
+                    return $"{answer} dollars";
+                }
+                else
+                {
+                    return $"{answer} dollar";
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using MethodOverloading;
+
+Console.WriteLine(Methods.Add(2, 3));
+Console.WriteLine(Methods.Add(2.45, 3.4));
+Console.WriteLine(Methods.Add(1, 0, true));
